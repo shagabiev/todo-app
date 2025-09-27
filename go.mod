bbeed249526b5cb087cac3208364861663c5fc00
Amir Shagabiev <amir.shagabiev@inbox.ru>
2025-09-27 15:53:16 +0300

    add authentication users

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ toolchain go1.24.7
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.21.0
 )
 
@@ -25,7 +27,6 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
---
+++ pkg/service/auth.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+
+	"github.com/shagabiev/todo-app"
+	"github.com/shagabiev/todo-app/pkg/repository"
+)
+
+const salt = "hjgrhjqw124617ajfhajs"
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
